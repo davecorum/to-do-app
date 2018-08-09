b4fdfcf2e55a24ef8ea4ca23916800d158f7da68
David Corum <nrgdavid710@gmail.com>
2018-08-08 18:52:02 -0700

    Todo App Worked On Some...

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+let body = document.querySelector("body");
+
+let todoForm = document.forms.login;
+let taskInput = todoForm.task.value;
+let descInput = todoForm.desc.value;
+
+function Task(task, desc) {
+  this.task = task;
+  this.desc = desc;
+};
+
+function newTask(task, desc) {
+  for(let i = 0, i < 3, i++) {
+    let firstDiv = document.createElement("div");
+    firstDiv.classList.add("container", "text-centered");
+    body.appendChild(firstDiv);
+    let secondDiv = document.createElement("div");
+    secondDiv.classList.add("row");
+    firstDiv.appendChild(secondDiv);
+    let thirdDiv = document.createElement("div");
+    thirdDiv.classList.add("col-md-2");
+    secondDiv.appendChild(thirdDiv);
+    
+  }
+}
+
+todoForm.addEventListener("submit", e => {
+  e.preventDefault();
+  let Task = new Task(taskInput, descInput);
+
+  todoForm.reset();
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
